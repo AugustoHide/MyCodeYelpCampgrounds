d0a021697dadcca8486a96e7b5e5f3bf07985c44
AugustoHide <augustohide@gmail.com>
2021-11-22 11:10:44 -0400

    campgrounds and reviews routes

--- schemas.js
+++ schemas.js
@@ -6,6 +6,7 @@ module.exports.campgroundSchema = Joi.object({
         location: Joi.string().required(),
         image: Joi.string().required(),
         price: Joi.number().required().min(0),
+        description: Joi.string().required()
     }).required()
 })
 
